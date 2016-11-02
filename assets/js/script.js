ad514370fc62416cd2eeeab058cd5961688a9381
GRSLfr <grisel.paul@gmail.com>
2016-11-02 13:02:22 +0100

    Add files

---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+function c(){
+	document.getElementById("result").value = "";
+	
+}
+
+function zero(){
+	var keepBtn = document.getElementById("zero").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+
+function one(){
+	var keepBtn = document.getElementById("one").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+
+function two(){
+	var keepBtn = document.getElementById("two").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+function three(){
+	var keepBtn = document.getElementById("three").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+function four(){
+	var keepBtn = document.getElementById("four").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+function five(){
+	var keepBtn = document.getElementById("five").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+function six(){
+	var keepBtn = document.getElementById("six").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+function seven(){
+	var keepBtn = document.getElementById("seven").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+function eight(){
+	var keepBtn = document.getElementById("eight").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+function nine(){
+	var keepBtn = document.getElementById("nine").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + keepBtn;	
+}
+
+
+
+function egal(){
+		var saveValue = document.getElementById("result").value;
+	var finalResult = eval(saveValue);
+	document.getElementById("result").value = saveValue + " = " + finalResult;
+}
+
+function more(){
+	var keepBtn = document.getElementById("more").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + " " + keepBtn + " ";
+}
+
+function less(){
+	var keepBtn = document.getElementById("less").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + " " + keepBtn + " ";
+}
+
+function multiply(){
+	var keepBtn = document.getElementById("multiply").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + " " + keepBtn + " ";
+}
+
+function divide(){
+	var keepBtn = document.getElementById("divide").value;
+	var saveValue = document.getElementById("result").value;
+	document.getElementById("result").value = saveValue + " " + keepBtn + " ";
+}
+
